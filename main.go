2a62371a96ec91784451a86e4812b91b95d8b9f8
Fernando Álvarez <alvarez.calleja.fernando@gmail.com>
2014-03-06 19:08:08 +0100

    Add dynamic content

--- main.go
+++ main.go
@@ -5,6 +5,19 @@ import (
     "github.com/martini-contrib/render"
 )
 
+type Site struct {
+    Title       string
+    Description string
+}
+
+func NewSite() *Site {
+    title := "Lozoya Valley"
+    description := `The Lozoya Valley initiative aims to associate product and media professional
+to launch and adapt a technology and creative ecosystem within a rural, natural environment,
+encouraging a healthy and sustainable way of life.`
+    return &Site{title, description}
+}
+
 func main() {
     m := martini.Classic()
     m.Use(martini.Static("assets"))
@@ -12,8 +25,10 @@ func main() {
         Layout: "layout",
     }))
 
+    site := NewSite()
+
     m.Get("/", func(r render.Render) {
-        r.HTML(200, "index", nil)
+        r.HTML(200, "index", site)
     })
 
     m.Run()
